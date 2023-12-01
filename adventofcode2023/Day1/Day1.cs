b4fef3a916c3781bb50574b24c5142f189d551bc
Scott Evensen <sceven@microsoft.com>
2023-12-01 12:18:48 -0800

    Adding more changes

--- adventofcode2023/Day1/Day1.cs
+++ adventofcode2023/Day1/Day1.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -7,19 +6,21 @@ class Day1
 {
     static void Main()
     {
-        string numbers;
-         PuzzleRaw = File.ReadAllLines(@".\Puzzle.txt");
+        string[] PuzzleRaw = File.ReadAllLines(@".\Puzzle.txt");
         int PuzzleAnswer = 0;
 
         foreach (string line in PuzzleRaw)
         {
             Console.WriteLine($"Processing {line}");
-            numbers = new string(line.Where(char.IsDigit).ToArray());
-            char firstNumber = numbers[0];
-            char lastNumber = numbers; // ^ operator is used to get the last element
-            Console.WriteLine($"Answer for this line is {firstNumber}{lastNumber}");
-            PuzzleAnswer += int.Parse($"{firstNumber}{lastNumber}");
-            Console.WriteLine(PuzzleAnswer);
+            string numbers = new string(line.Where(char.IsDigit).ToArray());
+            if (!string.IsNullOrEmpty(numbers))
+            {
+                int firstNumber = numbers[0];
+                int lastNumber = numbers[numbers.Length - 1];
+                Console.WriteLine($"Answer for this line is {firstNumber}{lastNumber}");
+                PuzzleAnswer += int.Parse($"{firstNumber}{lastNumber}");
+                Console.WriteLine(PuzzleAnswer);
+            }
         }
 
         Console.WriteLine(PuzzleAnswer);
